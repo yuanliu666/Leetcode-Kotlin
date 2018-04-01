07f09cb9c870c5b7b7af4acbb1aaf80debf8eaf0
akak19193 <liuy966@gmail.com>
2018-03-31 22:34:39 -0500

    Fix.

--- src/main/kotlin/average_of_levels_in_binary_tree.kt
+++ src/main/kotlin/average_of_levels_in_binary_tree.kt
@@ -27,51 +27,7 @@ class Solution_average_of_levels_in_binary_tree {
         var cur: List<TreeNode> = listOf(root)
         while (cur.isNotEmpty()) {
             val next = mutableListOf<TreeNode>()
-            val values = mutableListOf<Int>()
-            for (n in cur) {
-                // note: change n.value to n.`val` if you're using this in Leetcode OJ
-                values.add(n.value)
-                n.left?.let {
-                    next.add(it)
-                }
-                n.right?.let {
-                    next.add(it)
-                }
-            }
-            ret.add(getAverage(values))
-            cur = next
-        }
-        return ret.toDoubleArray()
-    }
-
-    // I am surprised that in LC discussion no one considers that the sum could overflow?
-    // to avoid this we use (n0 + n1 +... )/size = n0/size + n1/size + ...
-    // but then we face the problem of precision loss
-    // so we only do integer divide and use a parameter to accumulate remainder and also prevent it from overflow
-    // at last we do double divide and add them up
-    private fun getAverage(nums: List<Int>): Double {
-        var ret = 0.0
-        var remainder = 0
-        for (n in nums) {
-            ret += n / nums.size
-            remainder += n % nums.size
-            if (remainder >= nums.size) {
-                ret += remainder / nums.size
-                remainder %= nums.size
-            }
-        }
-        return ret + (remainder.toDouble() / nums.size)
-    }
-
-    // solution that does not consider sum overflow
-    fun averageOfLevels2(root: TreeNode?): DoubleArray {
-        val ret = mutableListOf<Double>()
-        if (root == null) {
-            return ret.toDoubleArray()
-        }
-        var cur: List<TreeNode> = listOf(root)
-        while (cur.isNotEmpty()) {
-            val next = mutableListOf<TreeNode>()
+            // sum could overflow as int so we use double
             var values = 0.0
             for (n in cur) {
                 // note: change n.value to n.`val` if you're using this in Leetcode OJ
