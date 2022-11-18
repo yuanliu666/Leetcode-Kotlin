f2582c37f373e108a6a149b484deed390840f64b
YuanLiu <13328332+yuanliu666@users.noreply.github.com>
2022-11-18 14:24:39 -0800

    Add description.

--- src/main/kotlin/pacific-atlantic-water-flow.kt
+++ src/main/kotlin/pacific-atlantic-water-flow.kt
@@ -1,6 +1,21 @@
 import java.util.*
-
-class `pacific-atlantic-water-flow` {
+/**
+ * There is an m x n rectangular island that borders both the Pacific Ocean and Atlantic Ocean.
+ * The Pacific Ocean touches the island's left and top edges, and the Atlantic Ocean touches the island's right and bottom edges.
+ * The island is partitioned into a grid of square cells.
+ * You are given an m x n integer matrix heights where heights[r][c] represents the height above sea level of the cell at coordinate (r, c).
+ * The island receives a lot of rain, and the rain water can flow to neighboring cells directly north, south, east, and west
+ * if the neighboring cell's height is less than or equal to the current cell's height.
+ * Water can flow from any cell adjacent to an ocean into the ocean.
+ * Return a 2D list of grid coordinates result where result[i] = [ri, ci] denotes that
+ * rain water can flow from cell (ri, ci) to both the Pacific and Atlantic oceans.
+ *
+ * m == heights.length
+ * n == heights[r].length
+ * 1 <= m, n <= 200
+ * 0 <= heights[r][c] <= 10^5
+ */
+class Solution_pacific_atlantic_water_flow {
 
     // This is very useful for writing less code
     private val dir = listOf(listOf(0, 1), listOf(1, 0), listOf(0, -1), listOf(-1, 0))
